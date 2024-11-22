4c00cd27a82f581665df51459ceb539bea3ac57c
Erico Labare <erico.labare@outlook.com>
2024-11-22 14:36:50 +0100

    commit tp4 + correction tp3

--- src/main/java/fr/diginamic/hello/restControleurs/HelloRestControleur.java
+++ src/main/java/fr/diginamic/hello/restControleurs/HelloRestControleur.java
@@ -1,6 +1,7 @@
 package fr.diginamic.hello.restControleurs;
 
 import fr.diginamic.hello.services.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/hello")
 public class HelloRestControleur {
 
+    @Autowired
     private HelloService helloService;
 
     public HelloRestControleur(HelloService helloService) {
--- src/main/java/fr/diginamic/hello/restControleurs/VilleRestControleur.java
+++ src/main/java/fr/diginamic/hello/restControleurs/VilleRestControleur.java
@@ -1,9 +1,9 @@
 package fr.diginamic.hello.restControleurs;
 
 import fr.diginamic.hello.entities.Ville;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,13 +12,27 @@ import java.util.List;
 @RequestMapping("/villes")
 public class VilleRestControleur {
 
-    @GetMapping
-    public List<Ville> getVilles() {
-        List<Ville> villes = new ArrayList<>();
+    private final List<Ville> villes = new ArrayList<>();
+
+    public VilleRestControleur() {
         villes.add(new Ville("Paris", 2148000));
         villes.add(new Ville("Lyon", 513000));
         villes.add(new Ville("Marseille", 861000));
         villes.add(new Ville("Toulouse", 479000));
+    }
+
+    @GetMapping
+    public List<Ville> getVilles() {
         return villes;
+    }
+
+    @PostMapping
+    public ResponseEntity<String> ajouterVille(@RequestBody Ville nouvelleVille) {
+        boolean existeDeja = villes.stream().anyMatch(ville -> ville.getNom().equalsIgnoreCase(nouvelleVille.getNom()));
+        if (existeDeja) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("La ville existe déjà");
+        }
+        villes.add(nouvelleVille);
+        return ResponseEntity.ok("Ville insérée avec succès");
     }
 }
