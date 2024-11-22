6273b260d2ba1f098a911f306f64a7825fdc99b4
Erico Labare <erico.labare@outlook.com>
2024-11-22 12:14:45 +0100

    commit tp1

---
+++ src/main/java/fr/diginamic/hello/restControleurs/HelloRestControleur.java
@@ -0,0 +1,14 @@
+package fr.diginamic.hello.restControleurs;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloRestControleur {
+    @GetMapping
+    public String direHello() {
+        return "Hello";
+    }
+}
